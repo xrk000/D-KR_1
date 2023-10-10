f0c5250edd116f033ed7f974b96763299c3f8d3b
xrk000 <144237985+xrk000@users.noreply.github.com>
2023-10-10 09:24:20 +0300

    Add files via upload

---
+++ D_KR_Nikulin.A.I..pas
@@ -0,0 +1,13 @@
+﻿program zad1;
+var x,y:real;
+begin
+  x:=-12;
+  while x<=0 do
+  begin
+    if x < -10 then y:= 74 * 65 - Sin(x)
+    else if (x >= -10) and (x < -2) then y := 36 - Sin(x)
+    else y := (Sin(x) / Log10(x)) * (Log10(x) / Cos(2 * x));
+    writeln('y в точке ',x:2:2,' равно ',y:2:2);
+    x:=x+0.3;
+  end;
+end.
